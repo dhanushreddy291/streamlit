a2fd30c29a96e939d3623ccbf3433eb70413e99e
Dhanush Reddy <29dhanushreddy@gmail.com>
2023-02-01 20:48:26 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/Users/dhanushreddy29/Desktop/2nd year all/VS Code/Projects/AI/streamlit/venv/bin/python
+
+import streamlit as st
+
+st.write(
+    """
+# My First App using Streamlit
+Hello *world!*
+"""
+)
